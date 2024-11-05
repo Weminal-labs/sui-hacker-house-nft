fbd4a7e618e18f8c62296814041de0d6730d013d
FromSunNews <phuongtu590@gmail.com>
2024-11-05 21:17:41 +0700

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <p className='h-auto w-auto'>Build something</p>
+    </div>
+  )
+}
+
+export default App
